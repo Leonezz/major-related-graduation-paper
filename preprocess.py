5a4d81dc9af20cbdb590ddb21be4f46bfab95086
zhuwenq <lonelywolf12138@outlook.com>
2021-05-11 21:46:22 +0800

    citekey and chapter1

--- preprocess.py
+++ preprocess.py
@@ -10,6 +10,6 @@ if len(md_file_list) != 0:
         with open(md_file, 'r', encoding='UTF-8') as f:
             content+=f.read().replace("---\nbibliography: [../ref.bib]\n---", "\n")
     result_file = open("./input.md", 'w', encoding='UTF-8')
-    result_file.write("---\nbibliography: [../ref.bib]\n---\n")
+    result_file.write("---\nbibliography: [./ref.bib]\n---\n")
     result_file.write(content)
     result_file.close()
